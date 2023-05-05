979a9b6412a86e9ce0770d15fd60cdc3e71cf3fa
fcroscatto <fcroscatto@gmail.com>
2023-05-04 21:11:48 -0300

    aula-operadores

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,2 +1,14 @@
 // EXERCICIO DE FIXAÇÃO
 
+let suaIdade = prompt("Digite sua idade")
+let idadeSeuAmigo = prompt("Qual idade do seu melhor amigo")
+let diferencaIdade1 = prompt("Sua idade é maior que a do seu melhor amigo")
+console.log(suaIdade)
+console.log(idadeSeuAmigo)
+console.log(diferencaIdade1)
+diferencadeIdade = suaIdade >= idadeSeuAmigo
+console.log(diferencadeIdade) 
+
+let diferença3 = suaIdade - idadeSeuAmigo
+console.log(diferença3)
+
